498c14594f1772d0527606168b61d1596182092e
yuzgit <ofnmk.xcode.05@gmail.com>
2022-07-03 14:13:04 +0900

    カラーリプレイス

--- main.go
+++ main.go
@@ -3,6 +3,6 @@ package goPackageTraining
 var defalt string = "\u001b[0m"
 
 func Red(text string) string {
-	result := "\u001b[31m" + text + defalt
+	result := "\u001b[34m" + text + defalt
 	return result
 }
